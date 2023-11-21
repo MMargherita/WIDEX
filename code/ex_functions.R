9b486f6ab2ee411e597236bb2a72fa7170dd601c
timriffe <tim.riffe@gmail.com>
2023-11-21 10:35:21 +0100

    made some progress writing functions, these use some advanced (and therefore ugly) tidyverse logic.

--- code/ex_functions.R
+++ code/ex_functions.R
@@ -8,6 +8,8 @@
 # Note: UW and WU should be structural 0s
 library(readODS)
 library(tidyverse)
+library(collapse)
+library(tidyfast)
 tmat <- read_ods("output/output_17_11_23/trans_mat/trans_mat_gen1_0204_high.ods")
 
 init <- c(P=.86,U=.1,W = .04)
@@ -15,44 +17,117 @@ init <- c(P=.86,U=.1,W = .04)
 p_tibble <-
   tmat |> 
   rename(age = rownames) |> 
-  filter(age > 64) |> 
-  pivot_longer(X_p11:X_p34, names_to = "from_to", values_to = "p") |> 
+  # TR: double check this age filter, might need one age higher?
+  filter(between(age, 65, 111)) |> 
+  pivot_longer(X_p11:X_p34, names_to = "transition", values_to = "p") |> 
   mutate(p = ifelse(is.na(p),0,p),
-         from_to = gsub("X_p","", from_to),
-         from_to = gsub("1","P", from_to),
-         from_to = gsub("2","U", from_to),
-         from_to = gsub("3","W", from_to),
-         from_to = gsub("4","D", from_to)) |> 
-  # filter(grepl("D",from_to)) |> 
-  # ggplot(aes(x = age, y = p, color = from_to)) +
-  # geom_line()
-  pivot_wider(names_from = "from_to", values_from = p)
+         transition = gsub("X_p","", transition),
+         transition = gsub("1","P", transition),
+         transition = gsub("2","U", transition),
+         transition = gsub("3","W", transition),
+         transition = gsub("4","D", transition),
+         from = substr(transition,1,1),
+         to = substr(transition,2,2)) |> 
+  select(-transition) 
 
-transition_block <- function(p_tibble, transition = "PP"){
+# rather flexible, defaults refer to use inside nested data.frame,
+# but it can also return a matrix proper if matrix == TRUE
+transient_block <- function(p_tibble, from = "P", to = "P", matrix = FALSE){
+  # TR, this function is rethought to be subservient to the  
+  # transient_matrix function, needs data.frame output, not matrix,
+  # don't bother w rownames. 
+  # Needed leading 0s for ages for "alphabetization",
+  if (matrix){
+  from <- from[1]
+  to   <- to[1]
   # TR: we can make this more computationally efficient if it becomes necessary
-  p_chunk <-
+  p_tibble <-
     p_tibble |> 
-    select(age_from = age,
-           transition = all_of(transition)) |> 
-    mutate(age_to = age_from + 1) 
+    # TR: for the record, I hate this sort
+    # of construct
+    filter(from == (!!from),
+           to == (!!to)) 
+  }
+  all_age <- min(p_tibble$age):(max(p_tibble$age)+1)
   
-  all_age <- c(p_chunk$age_from, p_chunk$age_to) |> unique()
+  if (matrix){
+    block <-
+      p_tibble |> 
+      fselect(age_from = age, 
+              p,
+              from,
+              to) |> 
+      fmutate(age_to = age_from + 1) |> 
+      complete(age_from = all_age,
+               age_to = all_age,
+               from, 
+               to,
+               fill = list(p = 0)) |> 
+      fmutate(age_to = sprintf("%03d", age_to),
+              age_from = sprintf("%03d", age_from),
+              age_to = paste(to, age_to, sep = "::"),
+              age_from = paste(from, age_from, sep = "::"))  |> 
+      dt_pivot_wider(names_from = age_from,
+                     values_from = p) |> 
+      fselect(-to,-from) |> 
+      column_to_rownames("age_to") |> 
+      as.matrix()
+  } else {
+    block <-
+      p_tibble |> 
+      fselect(age_from = age, 
+              p,
+              from,
+              to) |> 
+      fmutate(age_to = age_from + 1) |> 
+      complete(age_from = all_age,
+               age_to = all_age,
+               from, 
+               to,
+               fill = list(p = 0)) |> 
+      fmutate(age_to = sprintf("%03d",age_to),
+              age_from = sprintf("%03d",age_from),
+              age_to = paste(to,age_to,sep="::"))  |> 
+      dt_pivot_wider(names_from = age_from,
+                  values_from = p) |> 
+      fselect(-to,-from)
+  }
+  block
+}
+
+transient_matrix <- function(p_tibble, transient_states = c("P","U","W")){
+  # to be continued
   
-  p_chunk |> 
-    complete(age_from = all_age,
-             age_to = all_age,
-             fill = list(transition = 0)) |> 
-    pivot_wider(names_from = age_from,
-                values_from = transition)  |> 
-    column_to_rownames("age_to") |> 
+  # from_to_list <- outer(states,
+  #                       states,
+  #                       paste0) |> 
+  #   sort()
+  
+  # a hack until i have a better solution
+  age_name_keep <- paste0(transient_states[[1]],"::age_to")
+  
+  p_tibble |> 
+    filter(to %in% transient_states) |> 
+    arrange(from, to, age) |> 
+    group_by(from, to) |> 
+    # use this because grouping variables kept inside!
+    group_nest(keep = TRUE) |> 
+    mutate(data = map(data, ~.x |> 
+                        transient_block(matrix=FALSE) )) |> 
+    # arrange blocks into U configuration
+    pivot_wider(names_from = from, values_from = data) |> 
+    # stacks blocks in column
+    unnest(cols = all_of(states),names_sep="::") |> 
+    ungroup() |> 
+    # everything else is hackishness to get destination::age in rownames
+    select(-to) |> 
+    rename(to = (!!age_name_keep)) |> 
+    select(-ends_with("age_to")) |> 
+    column_to_rownames("to") |> 
     as.matrix()
 }
 
-transient_matrix <- function(p_tibble, states = c("P","U","W")){
-  # to be continued
-}
-
-
-
+transient_block(p_tibble)
+transient_matrix(p_tibble)
 
 
