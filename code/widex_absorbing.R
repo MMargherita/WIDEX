d1140a9751b964d1c12125bc7c7332a5fe12fc0c
timriffe <timriffe@gmail.com>
2024-10-03 16:39:09 +0200

    adds code to check partner mort

--- code/widex_absorbing.R
+++ code/widex_absorbing.R
@@ -1,5 +1,8 @@
 # TODO: current the prev estimates are all identical for different education combinations
-
+# we fixed the prev smoothing code to rerun on fiona 
+# also in the following, the partner mortality is far too low, so we need to check
+# how it was generated. Spot-checking against like-sex own-mortality we see 
+# too-large differences
 
 # we are going to use some functions from "ex_functions.R"
 source("code/ex_functions.R")
@@ -92,12 +95,17 @@ widex_vec_to_ex <- function(widex_vec){
 }
 
 
-p_tibble <- read_csv("output/trans_mat_res/tmat_gen2_0204_high.ods") |> 
+p_tibble <- read_csv("output/trans_mat_res/tmat_gen2_0204_low.ods") |> 
   filter(age < 112)
 
 p_partner <- read_csv("output/output_13_12_2023/pr_d_20plus/pr_d_20plus_0204_gen1_low.txt") |> 
   select(age_pw = ika, 
          PW = 3)
+# p_partner_check <- read_csv("output/trans_mat_res/tmat_gen1_0204_low.ods") |> 
+#   filter(age < 112) |> 
+#   select(age_pw=age, PD)
+# left_join(p_partner, p_partner_check, by = join_by(age_pw)) |> View()
+
 
 age_diffs <- -50:50
 prev <-
@@ -106,7 +114,7 @@ prev <-
   complete(age_diff = age_diffs, 
            edu_shsp, 
            fill = list(prev_hat = 0)) |> 
-  filter(edu_shsp == "high_low") |>
+  filter(edu_shsp == "low_low") |>
   rename(prev = prev_hat) |> 
   mutate(age_pw = 65 + age_diff)
 
@@ -143,3 +151,14 @@ widex_pars_to_ex(PW = p_partner$PW,
                    age_pw = p_partner$age_pw,
                    prev = previ) |> widex_vec_to_ex()
 }
+
+read_csv("output/output_13_12_2023/prev_age_diff/pred_prev_0204_gen2.txt") |> 
+  distinct() |> 
+  complete(age_diff = age_diffs, 
+           edu_shsp, 
+           fill = list(prev_hat = 0)) |> 
+  group_by(edu_shsp) |> 
+  mutate(prev = prev_hat/sum(prev_hat)) |> 
+  ggplot(aes(x = age_diff, y = prev, color = edu_shsp)) +
+  geom_line() +
+  scale_y_log10()
